04f9042174d1a0abf17dc5fff454fd6d638564d1
Scarlett Vu <uscarlettv@gmail.com>
2023-01-22 03:26:43 -0800

    Initial Commit

---
+++ assignment1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment1
+//
+//  Created by Scarlett  on 1/22/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
